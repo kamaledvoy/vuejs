74d65467708559a00456588f6e8037b85d365f85
kamalEdvoy <kamal@edvoy.com>
2021-08-13 18:56:22 +0530

    route test app

---
+++ routetestapp/src/views/Account.vue
@@ -0,0 +1,22 @@
+<template>
+    <h1>My Account page</h1>
+
+    <h2>{{ $route.params.id }}</h2>
+
+    <h5>Router</h5>
+    <p>{{ $router }}</p>
+
+    <h5>Route</h5>
+    <p>{{ $route }}</p>
+    
+</template>
+
+<script>
+export default {
+    name: 'Account'
+}
+</script>
+
+<style>
+
+</style>
